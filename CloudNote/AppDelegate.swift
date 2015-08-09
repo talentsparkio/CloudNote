817b32c0afb3ccfe7157ac1a065df7b61763716d
Nick Chen <nchen.dev@mac.com>
2015-08-08 20:53:02 -0700

    Initial import

---
+++ CloudNote/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  AppDelegate.swift
+//  CloudNote
+//
+//  Created by Nick Chen on 8/8/15.
+//  Copyright © 2015 TalentSpark. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // TODO: You need to enter your own appId and clientKey
+        Parse.setApplicationId("...", clientKey: "...")
+        
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+    }
+
+}
+
---
+++ CloudNote/CloudNote-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  CloudNote-Bridging-Header.h
+//  CloudNote
+//
+//  Created by Nick Chen on 8/8/15.
+//  Copyright © 2015 TalentSpark. All rights reserved.
+//
+
+#ifndef CloudNote_Bridging_Header_h
+#define CloudNote_Bridging_Header_h
+
+#import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
+#import <Bolts/Bolts.h>
+
+#endif /* CloudNote_Bridging_Header_h */
