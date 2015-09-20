6fa79894b9aaf68d06d146ca96dba9fa1a6fa4de
Nick Chen <nchen.dev@mac.com>
2015-09-20 00:02:01 -0700

    Simplify layout in edit mode
    Still having issues getting the scrollview to slide up on keyboard.
    Leaving as-is for now.

--- CloudNote/NoteViewController.swift
+++ CloudNote/NoteViewController.swift
@@ -9,24 +9,30 @@
 import UIKit
 
 class NoteViewController: UIViewController, UINavigationControllerDelegate, UITextFieldDelegate, UIImagePickerControllerDelegate {
-    
+
     @IBOutlet weak var noteTitle: UITextField!
     @IBOutlet weak var noteBody: UITextView!
     @IBOutlet weak var noteImage: UIImageView!
     @IBOutlet weak var saveButton: UIBarButtonItem!
-    
+    @IBOutlet weak var createdAt: UILabel!
+    @IBOutlet weak var updatedAt: UILabel!
+
     var note: PFObject?
     var didSelectImage = false
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         noteTitle.delegate = self
-        
+
         if let note = note {
+            let formatter = NSDateFormatter()
+            formatter.dateStyle = NSDateFormatterStyle.ShortStyle
+            formatter.timeStyle = .ShortStyle
+
             noteTitle.text   = note["Title"] as? String
             noteBody.text = note["Body"] as? String
-            
+
             let image = note["Photo"] as? PFFile
             image?.getDataInBackgroundWithBlock {(imageData, error) -> Void in
                 if error == nil {
@@ -36,33 +42,50 @@ class NoteViewController: UIViewController, UINavigationControllerDelegate, UITe
                 }
             }
         }
-        
+
         // Don't allow saving without title
         if noteTitle.text!.isEmpty {
             saveButton.enabled = false
         }
 
+        populateDateAsNecessary()
+
     }
-    
+
+    func populateDateAsNecessary() {
+        if let note = note {
+            let formatter = NSDateFormatter()
+            formatter.dateStyle = NSDateFormatterStyle.ShortStyle
+            formatter.timeStyle = .ShortStyle
+            if let created = note.createdAt, updated = note.updatedAt {
+                createdAt.text = "Created at: " + formatter.stringFromDate(created)
+                updatedAt.text = "Updated at: " + formatter.stringFromDate(updated)
+            }
+        } else {
+            createdAt.text = ""
+            updatedAt.text = ""
+        }
+    }
+
     // MARK: - Navigation
-    
+
     @IBAction func cancel(sender: UIBarButtonItem) {
         let isPresentingInAddNoteMode = presentingViewController is UINavigationController
-        
+
         if isPresentingInAddNoteMode {
             dismissViewControllerAnimated(true, completion: nil)
         } else {
             navigationController!.popViewControllerAnimated(true)
         }
     }
-    
+
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if saveButton === sender {
             if note == nil {
                 let newNote = PFObject(className:"Note")
                 note = newNote
             }
-            
+
             if let note = note {
                 note["Title"] = noteTitle.text
                 note["Body"] = noteBody.text
@@ -77,61 +100,61 @@ class NoteViewController: UIViewController, UINavigationControllerDelegate, UITe
             }
         }
     }
-    
+
     // MARK: UITextFieldDelegate
-    
+
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         // Hide the keyboard.
         textField.resignFirstResponder()
         return true
     }
-    
+
     func textFieldDidEndEditing(textField: UITextField) {
         checkValidTitle()
         navigationItem.title = textField.text
     }
-    
+
     func textFieldDidBeginEditing(textField: UITextField) {
         // Disable the Save button while editing.
         saveButton.enabled = false
     }
-    
+
     func checkValidTitle() {
         // Disable the Save button if the text field is empty.
         let text = noteTitle.text ?? ""
         saveButton.enabled = !text.isEmpty
     }
-    
+
     // MARK: UIImagePickerControllerDelegate
-    
+
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         // Dismiss the picker if the user canceled.
         dismissViewControllerAnimated(true, completion: nil)
     }
-    
+
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         // The info dictionary contains multiple representations of the image, and this uses the original.
         let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-        
+
         // Set photoImageView to display the selected image.
         noteImage.image = selectedImage
-        
+
         // Dismiss the picker.
         dismissViewControllerAnimated(true, completion: nil)
     }
-    
+
     @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
         didSelectImage = true
-        
+
         // Hide the keyboard.
         noteTitle.resignFirstResponder()
-        
+
         // UIImagePickerController is a view controller that lets a user pick media from their photo library.
         let imagePickerController = UIImagePickerController()
-        
+
         // Only allow photos to be picked, not taken.
         imagePickerController.sourceType = .PhotoLibrary
-        
+
         // Make sure ViewController is notified when the user picks an image.
         imagePickerController.delegate = self
         
